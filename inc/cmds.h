0e780c8a6f0ce51d8c994d7c324eb9040a4e6244
l0verflow <8ballol@proton.me>
2025-05-31 19:09:38 -0400

    Create cmds.h

---
+++ inc/cmds.h
@@ -0,0 +1,11 @@
+#ifndef PSY_CMDS_H
+#define PSY_CMDS_H
+
+#include <linux/types.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+
+bool cInstall(void);
+void cUninstall(void);
+
+#endif
